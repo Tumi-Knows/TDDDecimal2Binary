158ff384dcc309ffc1a9a37dd17a4c63555aff93
Tumi <tumi.knows@gmail.com>
2018-06-23 19:42:56 +0200

    Can't refactor further, it seems.
    I tried to further refactor the code. But tests broke. But because TDD... I simply hit Ctrl + Z to return to a stage where my code passes all its tests again :-)

--- src/main/java/binaryconverter/BinaryConverter.java
+++ src/main/java/binaryconverter/BinaryConverter.java
@@ -8,25 +8,25 @@ package binaryconverter;
 class BinaryConverter {
 
     public String convert(int n) {
-        
+
         String binaryString = "";
         final int divisor = 2; //2 is the common factor of all binary numbers
         int result = n;
-        int remainder; 
-        
+        int remainder;
+
         if (result != 0) {
             while(result >= 1){
                 remainder = result % divisor;
                 if(remainder == 1)
                     binaryString = "A".concat(binaryString);
                 else
-                   binaryString = "B".concat(binaryString);
+                    binaryString = "B".concat(binaryString);
                 result = result / divisor;
-            }   
+            }
         } else {
             binaryString = "B";
         }
-        
+
         return binaryString;
     }
 
